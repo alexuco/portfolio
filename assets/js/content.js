fd8e696f669e33752fec8883a0b4bbb566975f83
alexuco <alexuco.salsa@gmail.com>
2021-02-04 13:35:19 +0100

    footer YEAR auto updated

--- assets/js/content.js
+++ assets/js/content.js
@@ -10,16 +10,22 @@ async function getComponents() {
       console.log(error);
     });
 
-  await fetch("./assets/footer.html")
-    .then(response => {
-      return response.text();
-    })
-    .then(data => {
-      document.querySelector("footer").innerHTML = data;
-    })
-    .catch(error => {
-      console.log(error);
-    });
+  // await fetch("./assets/footer.html")
+  //   .then(response => {
+  //     return response.text();
+  //   })
+  //   .then(data => {
+  //     let year = new Date().getFullYear();
+  //     document.querySelector("footer").innerHTML = year + data;
+  //   })
+  //   .catch(error => {
+  //     console.log(error);
+  //   });
+    let create = document.createElement("small");
+    create.className = "copyright";
+    create.innerText = "Copyright 2019 - " + new Date().getFullYear() + " @Alex Martin";
+    document.querySelector("footer").appendChild(create);
+    
 }
 
 getComponents();
